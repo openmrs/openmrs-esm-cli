2e7f98393b3e11e6517aa9c1a49e94204fdea28c
Brandon Istenes <bistenes@gmail.com>
2020-03-15 08:54:31 -0600

    First attempt, using webpacked package. Didn't work.

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const version = require("../package.json").version;
+const autodoc = require("../lib/autodoc.js");
+
+program.version(version);
+program.option("-o, --outfile <outfile.md>", "change the output file", "README.md");
+program.option(
+  "-p, --package <package>",
+  "path to package.json file",
+  "package.json"
+);
+program.option(
+  "--cwd <cwd>",
+  "override the path to use as the current working directory"
+);
+program.option("-v, --verbose", "print lots of stuff");
+
+program.parse(process.argv);
+
+program.log = (...args) => console.log(...args);
+
+autodoc(program)
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
