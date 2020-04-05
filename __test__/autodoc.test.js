d1fddd5a7e6440f70b9dde80fa19986c4a874fd6
Brandon Istenes <bistenes@gmail.com>
2020-04-04 22:13:25 -0700

    Extract validators

--- __test__/autodoc.test.js
+++ __test__/autodoc.test.js
@@ -2,7 +2,45 @@ import { execSync } from "child_process";
 import fs from "fs";
 import rimraf from "rimraf";
 
-describe("autodoc", () => {
+describe("autodoc documentation", () => {
+  const tempDir = __dirname + "/temp";
+  const readmePath = tempDir + "/README.md";
+  const initialReadme = "# My Module\n\nIt is so beautiful\n";
+
+  let result;
+  beforeAll(() => {
+    rimraf.sync(tempDir);
+    fs.mkdirSync(tempDir);
+    fs.writeFileSync(readmePath, initialReadme);
+    const fixture = __dirname + "/fixtures/tsx-module.tsx";
+    execSync(`./bin/cli.js -e ${fixture} -o ${readmePath}`, {
+      encoding: "utf-8",
+    });
+    result = fs.readFileSync(readmePath, "utf-8");
+  });
+
+  it("includes the original content", () => {
+    expect(result).toMatch(initialReadme);
+  });
+
+  it("includes the name of the key", () => {
+    expect(result).toMatch(/-.*foo/);
+  });
+
+  it("includes out of the box validators", () => {
+    expect(result).toMatch(/- must be a string/i);
+    expect(result).toMatch(/- must be a boolean/i);
+  });
+
+  it("includes custom validators", () => {
+    expect(result).toMatch(/it's ok/);
+  });
+
+  test.todo("includes 'description' key");
+  test.todo("reasonable formatting for default arrays");
+});
+
+describe("autodoc capabilities", () => {
   const tempDir = __dirname + "/temp";
   const readmePath = tempDir + "/README.md";
   const initialReadme = "# My Module\n\nIt is so beautiful\n";
@@ -60,7 +98,4 @@ describe("autodoc", () => {
   });
 
   test.todo("can extract from an ES6 JS module");
-  test.todo("includes validator explanations");
-  test.todo("includes 'description' key");
-  test.todo("reasonable formatting for default arrays");
 });
